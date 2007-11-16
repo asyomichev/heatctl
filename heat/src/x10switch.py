907a755ad8f5323b67cb569fbe5dc382466761ab
alex <alex@fe2ca904-77f2-42e9-8506-07942c27b206>
2007-11-16 02:54:25 +0000

    implemented via heyu commands
    git-svn-id: file:///opt/repository/trunk@8 fe2ca904-77f2-42e9-8506-07942c27b206

--- heat/src/x10switch.py
+++ heat/src/x10switch.py
@@ -1,20 +1,32 @@
 from thermostat import HeaterStatusEvent
+import logging
 import os
 
 class X10Switch:
     """ Listens for heater status events and converts them into heyu commands """
     
     def __init__(self, queue, config):
+        self.testMode = (config.get("Furnace", "testMode") == "on")
+        self.x10id = config.get("Furnace", "x10id")
         self.subscriberId = queue.subscribe(self, "HeaterStatusEvent")
         self.queue = queue
+        self.logger = logging.getLogger("heat.furnace")
         
     def processEvent(self, event):
-        rc = os.system("heyu %s h1" % event.status);
+        rc = 0
+        if not self.testMode:
+            rc = os.system("heyu %s %s" % (event.status, self.x10id));
         if rc != 0:
             raise RuntimeException("Failed to send X10 command")
+        self.logger.info(event.status)
+        self.lastStatus = event.status
         
     def unsubscribe(self):
         self.queue.unsubscribe(self.subscriberId)
         
     def id(self):
         return "X10 Switch"
+
+    def status(self):
+        return "furnace status: %s" % self.lastStatus
+    
